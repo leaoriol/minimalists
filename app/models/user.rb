ab2ace98952cfd83c175303759cb1bc8b21e56d8
Lea Oriol <lea.oriol.bc@gmail.com>
2017-06-25 14:28:39 -0700

    login and sign up pages scss and html

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(email: "user@example.com", password: "test1234")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ''
+    assert_not @user.valid?
+  end
+
 end
