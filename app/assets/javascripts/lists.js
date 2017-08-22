93528c938ac13bd1d0b02f91fdb7c0b9257b659d
Lea Oriol <lea.oriol.bc@gmail.com>
2017-08-21 20:23:32 -0700

    search and actions filter working

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,8 +1,9 @@
 $(document).ready(function($) {
   console.log("hello");
-  var listItemsAttributes = $('.hidden-attribute');
 
   // SEARCH FUNCTIONALITY
+  var listItemsAttributes = $('.hidden-attribute');
+
   $('#search-filter').keyup(function() {
     listItemsAttributes.parent().hide();
     // get the string from the select input box
@@ -19,6 +20,54 @@ $(document).ready(function($) {
   });
 
   // FILTER FUNCTIONALITY
+  var actionOptions = [];
+  var listItemsActions = $('.hidden-action');
 
+  $('.dropdown-menu a').on('click', function(event) {
+
+    listItemsActions.parent().show();
+
+    var $target = $(event.currentTarget),
+        val = $target.attr('data-value'),
+        $input = $target.find('input'),
+        index;
+
+    if ((index = actionOptions.indexOf(val)) > -1 ) {
+      // action val is unchecked - hide corresponding items
+      actionOptions.splice(index, 1);
+      setTimeout(function() {
+        $input.prop('checked', false)
+      }, 0);
+      listItemsActions.each(function() {
+        var text = $(this).text().trim();
+        if(!actionOptions.includes(text)) {
+          $(this).parent().hide();
+        };
+      });
+    } else {
+      // indicate that the button was checked - show corresponding items
+      actionOptions.push(val);
+      setTimeout(function() {
+        $input.prop('checked', true)
+      }, 0);
+      listItemsActions.each(function() {
+        var text = $(this).text().trim();
+
+        // check if text (i.e. keep) is found in actionOptions
+
+        if(!actionOptions.includes(text)) {
+          $(this).parent().hide();
+        };
+      });
+    }
+
+    if (actionOptions.length == 0) {
+      listItemsActions.parent().show();
+    };
+
+    $(event.target).blur();
+
+    console.log(actionOptions);
+    return false
+  });
 });
-
