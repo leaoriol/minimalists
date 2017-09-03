98612d8b548da8817313063520045ce637c5edc2
Lea Oriol <lea.oriol.bc@gmail.com>
2017-09-03 11:26:32 -0700

    fix controller errors

--- test/controllers/goals_controller_test.rb
+++ test/controllers/goals_controller_test.rb
@@ -17,34 +17,40 @@ class GoalsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in @user
     get new_goal_url
     assert_response :success
   end
 
   test "should create goal" do
+    sign_in @user
     assert_difference('Goal.count') do
       post goals_url, params: { goal: { overall_goal: @goal.overall_goal, user_id: @goal.user_id } }
     end
-
-    assert_redirected_to goal_url(Goal.last)
+    assert_redirected_to goals_url
+    # assert_redirected_to goal_url(Goal.last)
   end
 
   test "should show goal" do
+    sign_in @user
     get goal_url(@goal)
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_goal_url(@goal)
-    assert_response :success
-  end
+  # edit goal is taken care of via ajax calls 
+  # test "should get edit" do
+  #   get edit_goal_url(@goal)
+  #   assert_response :success
+  # end
 
   test "should update goal" do
+    sign_in @user
     patch goal_url(@goal), params: { goal: { overall_goal: @goal.overall_goal, user_id: @goal.user_id } }
     assert_redirected_to goal_url(@goal)
   end
 
   test "should destroy goal" do
+    sign_in @user
     assert_difference('Goal.count', -1) do
       delete goal_url(@goal)
     end
